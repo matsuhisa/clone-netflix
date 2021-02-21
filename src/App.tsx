86d3e224dbdaaf6b1b3bac311625fb7cae7fc339
matsuhisa <matsuhisa+github@gmail.com>
2021-02-21 19:41:16 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,11 @@ function App() {
         fetchUrl={requests.feachNetflixOriginals}
         isLargeRow={true}
       />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row
+        title="feactDocumentMovies"
+        fetchUrl={requests.feachTrending}
+        isLargeRow={false}
+      />
     </div>
   );
 }
