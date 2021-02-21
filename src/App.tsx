6958db266cd8e58cb2deb8df79630ba077de319e
matsuhisa <matsuhisa+github@gmail.com>
2021-02-21 19:22:23 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Row } from './Row';
+import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow={true}
+      />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
